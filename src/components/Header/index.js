ff28aa0be38ded119bd33bf9a8c7b7a1a5a8d657
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-03-07 18:35:53 -0500

    Components of onlineShop

---
+++ src/components/Header/index.js
@@ -0,0 +1,33 @@
+import boxicons from 'boxicons';
+import React from 'react';
+
+import Logo from '../../assets/images/Nike.jpg'
+
+function Header() {
+    return (
+        <header>
+            {/* ***************************************************************  LOGO */}
+            <a href="#">
+                <div className="logo">
+                    <img src={ Logo } alt="Logo" width="150"/>
+                </div>
+            </a>
+            {/* ***************************************************************  MENU */}
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Products</a>
+                </li>
+            </ul>
+            {/* ***************************************************************  SHOPPING CART */}
+            <div className="cart">
+                <box-icon name="cart"></box-icon>
+                <span className="item__total">0</span>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Products/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListProducts = () => {
+    return (
+        <div>
+            <h1>List of products</h1>
+        </div>
+    )
+}
+
+export default ListProducts;
