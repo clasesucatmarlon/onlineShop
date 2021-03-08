27849e063d30207411c6bc2d3aab6b2a2b42b8cb
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-03-08 16:11:23 -0500

    Added card of products

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,11 +1,135 @@
-import React from 'react';
+import React from "react";
+import imageCard from "../../assets/images/img02.jpg";
 
 const ListProducts = () => {
     return (
-        <div>
-            <h1>List of products</h1>
-        </div>
-    )
-}
+        <>
+            <h1 className="title">PRODUCTS</h1>
+
+            <div className="products">
+
+                <div className="product">
+                    <a href="/#">
+                        <div className="product__img">
+                            <img src={imageCard} alt="" />
+                        </div>
+                    </a>
+                    <div className="product__footer">
+                        <h1>Title</h1>
+                        <p>Category</p>
+                        <p className="price">$245</p>
+                    </div>
+                    <div className="button">
+                        <button className="btn">
+                            Add to car
+                        </button>
+                        <div>
+                            <a href="/#" className="btn">
+                                View
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="product">
+                    <a href="/#">
+                        <div className="product__img">
+                            <img src={imageCard} alt="" />
+                        </div>
+                    </a>
+                    <div className="product__footer">
+                        <h1>Title</h1>
+                        <p>Category</p>
+                        <p className="price">$245</p>
+                    </div>
+                    <div className="button">
+                        <button className="btn">
+                            Add to car
+                        </button>
+                        <div>
+                            <a href="/#" className="btn">
+                                View
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="product">
+                    <a href="/#">
+                        <div className="product__img">
+                            <img src={imageCard} alt="" />
+                        </div>
+                    </a>
+                    <div className="product__footer">
+                        <h1>Title</h1>
+                        <p>Category</p>
+                        <p className="price">$245</p>
+                    </div>
+                    <div className="button">
+                        <button className="btn">
+                            Add to car
+                        </button>
+                        <div>
+                            <a href="/#" className="btn">
+                                View
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="product">
+                    <a href="/#">
+                        <div className="product__img">
+                            <img src={imageCard} alt="" />
+                        </div>
+                    </a>
+                    <div className="product__footer">
+                        <h1>Title</h1>
+                        <p>Category</p>
+                        <p className="price">$245</p>
+                    </div>
+                    <div className="button">
+                        <button className="btn">
+                            Add to car
+                        </button>
+                        <div>
+                            <a href="/#" className="btn">
+                                View
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="product">
+                    <a href="/#">
+                        <div className="product__img">
+                            <img src={imageCard} alt="" />
+                        </div>
+                    </a>
+                    <div className="product__footer">
+                        <h1>Title</h1>
+                        <p>Category</p>
+                        <p className="price">$245</p>
+                    </div>
+                    <div className="button">
+                        <button className="btn">
+                            Add to car
+                        </button>
+                        <div>
+                            <a href="/#" className="btn">
+                                View
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    );
+};
 
 export default ListProducts;
