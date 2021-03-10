2bdf522fe069e3235ca7da6eb14a68fac42d5603
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-03-10 12:38:23 -0500

    Changed the modal page and added responsive

--- src/components/Car/index.js
+++ src/components/Car/index.js
@@ -14,29 +14,75 @@ function Car() {
                 <h2>Your Car</h2>
 
                 <div className="car__center">
-
                     <div className="car__item">
                         <div className="image">
                             <img src={Card} alt="" />
                         </div>
                         <div className="title__price">
-                            <h3>Title</h3>
+                            <h3>Acer Nitro 5 Gaming, Core i5-9300H</h3>
                             <p className="price">$545</p>
                         </div>
-                        <div className="cant">
-                            <box-icon name="up-arrow" type="solid"></box-icon>
-                            <p className="cant">2</p>
-                            <box-icon name="down-arrow" type="solid"></box-icon>
-                        </div>
-                        <div className="remove__item">
-                            <box-icon name="trash" type="solid"></box-icon>
+                        <div className="cant__remove">
+                            <div className="cant">
+                                <box-icon name="up-arrow" type="solid"></box-icon>
+                                <p className="cant">2</p>
+                                <box-icon name="down-arrow" type="solid"></box-icon>
+                            </div>
+                            <div className="remove__item">
+                                <box-icon name="trash" type="solid"></box-icon>
+                            </div>
                         </div>
                     </div>
-
                 </div>
 
+                <div className="car__center">
+                    <div className="car__item">
+                        <div className="image">
+                            <img src={Card} alt="" />
+                        </div>
+                        <div className="title__price">
+                            <h3>Acer Nitro 5 Gaming, Core i5-9300H</h3>
+                            <p className="price">$545</p>
+                        </div>
+                        <div className="cant__remove">
+                            <div className="cant">
+                                <box-icon name="up-arrow" type="solid"></box-icon>
+                                <p className="cant">2</p>
+                                <box-icon name="down-arrow" type="solid"></box-icon>
+                            </div>
+                            <div className="remove__item">
+                                <box-icon name="trash" type="solid"></box-icon>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="car__center">
+                    <div className="car__item">
+                        <div className="image">
+                            <img src={Card} alt="" />
+                        </div>
+                        <div className="title__price">
+                            <h3>Acer Nitro 5 Gaming, Core i5-9300H</h3>
+                            <p className="price">$545</p>
+                        </div>
+                        <div className="cant__remove">
+                            <div className="cant">
+                                <box-icon name="up-arrow" type="solid"></box-icon>
+                                <p className="cant">2</p>
+                                <box-icon name="down-arrow" type="solid"></box-icon>
+                            </div>
+                            <div className="remove__item">
+                                <box-icon name="trash" type="solid"></box-icon>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
                 <div className="car__footer">
-                    <h3>Total: $456</h3>
+                    <h3>Total: <span>$456</span></h3>
                     <button className="btn">Payment</button>
                 </div>
 
